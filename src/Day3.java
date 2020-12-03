9cfb6f5eb2a3de5e226c87fe9929e4cf00e400f9
Robin <denhemligafilmen@gmail.com>
2020-12-03 21:05:20 +0100

    Day 3 Complete!

--- src/Day3.java
+++ src/Day3.java
@@ -6,15 +6,20 @@ public class Day3 {
     public static void main(String[] args) throws Exception {
         
         Scanner in = new Scanner(System.in);
-        int x = 0;
-        int x1 = 0;
-        int x2 = 0; 
-        int x3 = 0; 
-        int x4 = 0;
+        int x = 0; // R1D1
+        int x1 = 0; // R3D1
+        int x2 = 0; // R5D1
+        int x3 = 0; // R7D1
+        int x4 = 0; // R1D2
+        boolean varannan = true;
 
         int y = 0;
         char tree = '#';
         int trees = 0;
+        int trees1 = 0;
+        int trees2 = 0;
+        int trees3 = 0;
+        int trees4 = 0;
 
         while(!in.hasNext("!")){     
             String line = in.nextLine();
@@ -33,12 +38,45 @@ public class Day3 {
             */
 
             //Del B
+
+            //R1D1
+            if(arr[x] == tree){
+                trees++; 
+            }  
+            x = (x+1) % width;
+
+            //R3D1
+            if(arr[x1] == tree){
+                trees1++; 
+            }  
+            x1 = (x1+3) % width;
+
+            //R5D1
+            if(arr[x2] == tree){
+                trees2++; 
+            }  
+            x2 = (x2+5) % width;
+
+            //R7D1
+            if(arr[x3] == tree){
+                trees3++; 
+            }  
+            x3 = (x3+7) % width;
+
+            //R1D2
             
+            if(varannan){
+                if(arr[x4] == tree){
+                    trees4++; 
+                }  
+                x4 = (x4+1) % width;
+            }
+            varannan = !varannan;
 
         }
-        System.out.println("\n TREES: " +trees);
+        long product = trees * trees1 * trees2 * trees3 * trees4;
+        System.out.println("\n TREES: " +trees + ", " +trees1 + ", " +trees2 + ", " +trees3 + ", " +trees4 + "\n Product: " + product);
 
-       
     }
 
 }
